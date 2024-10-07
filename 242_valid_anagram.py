83ffc3c11d1c02c4051f29f1f524db770795a242
Brian Rice <riceb53@gmail.com>
2024-10-07 16:27:05 -0700

    242 valid anagram

---
+++ 242_valid_anagram.py
@@ -0,0 +1,22 @@
+def isAnagram(s,t):
+    s_dict = {}
+    t_dict = {}
+    if len(s) != len(t):
+        return False
+    for i, char in enumerate(s):
+        if s[i] in s_dict:
+            s_dict[s[i]] += 1
+        else:
+            s_dict[s[i]] = 1
+        
+        if t[i] in t_dict:
+            t_dict[t[i]] += 1
+        else:
+            t_dict[t[i]] = 1
+        
+    return s_dict == t_dict
+        
+        
+
+print(isAnagram("anagram", "nagaram") == True)
+print(isAnagram("rat", "car") == False)
